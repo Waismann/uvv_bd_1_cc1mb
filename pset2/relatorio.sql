f710bded9c02594f85cae2cd6f021be02a690799
Henrique Waisman <100635335+henriquewaisman@users.noreply.github.com>
2022-05-04 09:56:27 -0300

    Update relatorio.sql

--- pset2/relatorio.sql
+++ pset2/relatorio.sql
@@ -1,1 +1,12 @@
+select avg(f.salario) as media_salarial, d.nome_departamento
+from funcionario f
+inner join departamento d
+on d.numero_departamento = f.numero_departamento
+group by d.nome_departamento;
 
+
+
+
+select avg(f.salario) as media_salarial, f.sexo
+from funcionario f
+group by f.sexo;
