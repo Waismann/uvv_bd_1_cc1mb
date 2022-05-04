8bfdbe78b822aad88bc4d02c88483dfa6179d2d8
Henrique Waisman <100635335+henriquewaisman@users.noreply.github.com>
2022-05-04 15:53:03 -0300

    Update relatorio.sql

--- pset2/relatorio.sql
+++ pset2/relatorio.sql
@@ -14,8 +14,19 @@ group by f.sexo;
 select  d.nome_departamento,
 concat(primeiro_nome, " ", nome_meio, ". ", ultimo_nome) as nome_completo,
 f.data_nascimento,
-TIMESTAMPDIFF(year, data_nascimento, curdate()) as idade,
+timestampdiff(year, data_nascimento, curdate()) as idade,
 f.salario
 from departamento d
 inner join funcionario f
 on f.numero_departamento = d.numero_departamento;
+
+--4
+select concat(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) as nome_funcionario,
+f.data_nascimento,
+timestampdiff(year, data_nascimento, curdate()) as idade,
+f.salario as salario_atual,
+(case
+when (f.salario < 35000) then f.salario + (f.salario * 0.2)
+else f.salario + (f.salario * 0.15)
+end) as salario_ajustado
+from funcionario f;
