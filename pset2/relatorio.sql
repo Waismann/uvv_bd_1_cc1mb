30740a630871f254611504533fc4549ed3904f28
Henrique Waisman <100635335+henriquewaisman@users.noreply.github.com>
2022-05-09 20:05:12 -0300

    Update relatorio.sql

--- pset2/relatorio.sql
+++ pset2/relatorio.sql
@@ -178,4 +178,4 @@ inner join funcionario f
 where dp.numero_departamento = f.numero_departamento 
 and p.numero_projeto = t.numero_projeto 
 and t.cpf_funcionario = f.cpf
-order by f.nome_funcionario desc;
+order by f.primeiro_nome desc;
